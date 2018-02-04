fe008fb236f81b0a524f847ca037b5c8892b26e4
WillieWetz <wcwetzel@unomaha.edu>
2018-02-04 09:26:40 -0600

    work

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -80,12 +80,6 @@ WSGI_APPLICATION = 'efsblog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 DATABASES = {
      'default': {
